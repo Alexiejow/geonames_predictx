0c5172a1638194cef7267da4b75f1f1a28de8bcc
Aleksy Galkowski <aleksy.codes@gmail.com>
2025-02-26 22:53:14 +0100

    Change metro condition

--- src/pipeline_spark/transform_filters.py
+++ src/pipeline_spark/transform_filters.py
@@ -10,31 +10,35 @@ def filter_populated_places(df: DataFrame) -> DataFrame:
 
 def get_boundary_mask(df: DataFrame) -> Column:
     """
-    Returns a Spark Column representing a boolean mask for locations that should have boundaries.
-    This includes:
-      - All rows with feature_code in ["PPLA", "PPLA2", "PPLC"]
-      - Rows with feature_code "PPL" that have a population > 100000 and whose admin2_code 
-        is not in the set of admin2 codes from rows with feature_code in ["PPLC", "PPLA", "PPLA2"].
-    
-    Note: This function assumes that 'population' can be cast to integer.
+    Returns a Spark Column representing a boolean mask for identifying metropolitan areas.
+
+    **Metro Criteria:**
+      1. The city must have a population **≥ 10% of the largest city's population.**
+      2. If the city is `PPL`, it must **not be in the same `admin2_code` as any `PPLA` or `PPLC`** 
+         (to prevent large districts within cities, and cities in the same areas as administrative locations from being considered metros).
+
+    Assumes 'population' can be cast to integer.
     """
-    # Cast 'population' to integer.
     df = df.withColumn("population", F.col("population").cast("int"))
-    
-    # Condition for administrative centers: PPLA, PPLA2, PPLC.
-    condition_admin = F.col("feature_code").isin(["PPLA", "PPLA2", "PPLC"])
-    
-    # Collect admin2 codes from rows with feature_code in ["PPLC", "PPLA", "PPLA2"].
-    mask_admin_df = df.filter(F.col("feature_code").isin(["PPLC", "PPLA", "PPLA2"]))
-    admin2_list = mask_admin_df.select("admin2_code") \
-                               .where(F.col("admin2_code").isNotNull()) \
-                               .distinct() \
-                               .rdd.flatMap(lambda x: x) \
-                               .collect()
-    
-    # For PPL rows: population > 100000 and admin2_code not in the collected admin2_list.
-    condition_ppl = (F.col("feature_code") == "PPL") & \
-                    (F.col("population") > 100000) & \
-                    (~F.col("admin2_code").isin(admin2_list))
-    
-    return condition_admin | condition_ppl
+
+    # Find the maximum population in the dataset
+    max_population = df.agg(F.max("population")).collect()[0][0]
+    threshold_population = max_population * 0.1
+
+    # Collect set of admin2 codes from PPLA and PPLC locations
+    admin2_excluded = df.filter(F.col("feature_code").isin(["PPLA", "PPLC"])) \
+                        .select("admin2_code").distinct() \
+                        .where(F.col("admin2_code").isNotNull()) \
+                        .rdd.flatMap(lambda x: x).collect()
+
+    # General metro condition: Population >= 10% of max city
+    condition_population = F.col("population") >= threshold_population
+
+    # PPL additional exclusion (admin2 for PPLA/PPLC)
+    condition_ppl_exclusion = (F.col("population") >= threshold_population) & \
+                              (F.col("feature_code") == "PPL") & \
+                              (~F.col("admin2_code").isin(admin2_excluded))
+
+    # Allow all cities that meet the population threshold OR PPL cities that also satisfy the exclusion rule
+    return condition_population | condition_ppl_exclusion
+
